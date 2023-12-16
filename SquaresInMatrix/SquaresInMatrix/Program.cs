3a458b1f513ef929de5ce396ca04e87fc33082fe
Asen Popov <61016270+Apsie09@users.noreply.github.com>
2023-12-16 22:35:47 +0200

    Add files via upload

---
+++ SquaresInMatrix/SquaresInMatrix/Program.cs
@@ -0,0 +1,43 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] dimensions = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+        if (dimensions.Length < 2)
+        {
+            Console.WriteLine("Invalid input!");
+            return;
+        }
+        else
+        {
+            int rows = dimensions[0];
+            int cols = dimensions[1];
+
+            // Read matrix characters
+            char[][] matrix = new char[rows][];
+            for (int i = 0; i < rows; i++)
+            {
+                matrix[i] = Console.ReadLine().Split(' ').Select(char.Parse).ToArray();
+            }
+
+            int br = 0;
+
+            for (int i = 0; i < rows - 1; i++)
+            {
+                for (int j = 0; j < cols - 1; j++)
+                {
+                    if (matrix[i][j] == matrix[i][j + 1] && matrix[i][j] == matrix[i + 1][j] && matrix[i][j] == matrix[i + 1][j + 1])
+                    {
+                        br++;
+                    }
+                }
+            }
+
+            Console.WriteLine(br);
+        }
+
+
+
+    }
+}
