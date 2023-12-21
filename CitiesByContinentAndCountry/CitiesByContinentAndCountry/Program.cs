225f0b17fbcecd92d79194883fad90e0ba7eb20a
Asen Popov <61016270+Apsie09@users.noreply.github.com>
2023-12-21 13:51:31 +0200

    Add files via upload

---
+++ CitiesByContinentAndCountry/CitiesByContinentAndCountry/Program.cs
@@ -0,0 +1,40 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var cities = new Dictionary<string, Dictionary<string, List<string>>>();
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < n; i++)
+        {
+            var line = Console.ReadLine();
+            var parts = line.Split(' ');
+            var contintent = parts[0];
+            var country = parts[1];
+            var city = parts[2];
+
+            if (!cities.ContainsKey(contintent))
+            {
+                cities.Add(contintent, new Dictionary<string, List<string>>());
+            }
+
+            if (!cities[contintent].ContainsKey(country))
+            {
+                cities[contintent][country] = new List<string>();
+            }
+
+            cities[contintent][country].Add(city);
+        }
+
+        foreach (var continent in cities)
+        {
+            Console.WriteLine($"{continent.Key}:");
+
+            foreach (var country in continent.Value)
+            {
+                Console.WriteLine($"  {country.Key} -> {string.Join(", ", country.Value)}");
+            }
+        }
+
+    }
+}
