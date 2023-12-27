aff27889c020406c4cc9b2d717993d759097d1a9
Asen Popov <61016270+Apsie09@users.noreply.github.com>
2023-12-27 18:02:03 +0200

    Add files via upload

---
+++ EvenTimes/EvenTimes/Program.cs
@@ -0,0 +1,32 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        Dictionary<int, int> numbers = new Dictionary<int, int>();
+
+        for (int i = 0; i < n; i++)
+        {
+            int input = int.Parse(Console.ReadLine());
+
+            if (!numbers.ContainsKey(input))
+            {
+                numbers.Add(input, 0);
+            }
+            numbers[input]++;
+        }
+
+
+
+        foreach (var (key, value) in numbers)
+        {
+            if (value % 2 == 0)
+            {
+                Console.WriteLine(key);
+                break;
+            }
+        }
+
+    }
+}
