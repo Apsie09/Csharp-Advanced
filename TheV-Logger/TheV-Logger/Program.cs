ae0a76656f0b00bb84dbb468b94934eed657e06f
Asen Popov <61016270+Apsie09@users.noreply.github.com>
2023-12-27 18:07:37 +0200

    Add files via upload

---
+++ TheV-Logger/TheV-Logger/Program.cs
@@ -0,0 +1,79 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Dictionary structure  ------>
+        //Saffrona -> following.Add(....)
+        //Saffrona -> followers.Add(....)
+        var vlogger = new Dictionary<string, Dictionary<string, HashSet<string>>>();
+
+        /* LOGIC ----->
+         if(!vlogger.ContainsKey("Saffrona"))
+         {
+             vlogger.Add("Saffrona", new Dictionary<string, HashSet<string>>());
+         }
+
+         vlogger["Saffrona"].Add("following",new HashSet<string>());
+         vlogger["Saffrona"].Add("followers", new HashSet<string>());
+
+         vlogger["Saffrona"]["following"].Add("Emil");*/
+
+        string input = Console.ReadLine();
+
+        string following = "following";
+        string followers = "followers";
+
+        while (input != "Statistics")
+        {
+            //Input --->
+            //EmilConrad joined The V-Logger
+            //VenomTheDoctor followed The V-Logger
+
+            string[] inputArgs = input.Split();
+
+            string action = inputArgs[1];
+            string user = inputArgs[0];
+            string starUser = inputArgs[2];
+
+            if (action == "joined" && !vlogger.ContainsKey(user))
+            {
+                vlogger.Add(user, new Dictionary<string, HashSet<string>>());
+                vlogger[user].Add(following, new HashSet<string>());
+                vlogger[user].Add(followers, new HashSet<string>());
+            }
+            else if (action == "followed" && vlogger.ContainsKey(user)
+                && vlogger.ContainsKey(starUser) && user != starUser)
+            {
+                vlogger[user][following].Add(starUser);
+                vlogger[starUser][followers].Add(user);
+            }
+
+            input = Console.ReadLine();
+        }
+
+        Console.WriteLine($"The V-Logger has a total of {vlogger.Count} vloggers in its logs.");
+
+        var sortedVloggers = vlogger.OrderByDescending(x => x.Value[followers].Count())
+            .ThenBy(x => x.Value[following].Count());
+
+        int counter = 1;
+
+        foreach (var currentVlogger in sortedVloggers)
+        {
+            Console.WriteLine($"{counter}. {currentVlogger.Key} : {currentVlogger.Value[followers].Count} followers, " +
+                $"{currentVlogger.Value[following].Count} following");
+
+            if (counter == 1)
+            {
+                foreach (var item in currentVlogger.Value[followers].OrderBy(x => x))
+                {
+                    Console.WriteLine($"*  {item}");
+                }
+            }
+
+            counter++;
+        }
+
+
+    }
+}
