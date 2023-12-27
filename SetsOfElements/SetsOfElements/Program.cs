a325b7b368e36fa498156d76c528704b0a5be092
Asen Popov <61016270+Apsie09@users.noreply.github.com>
2023-12-27 17:58:31 +0200

    Add files via upload

---
+++ SetsOfElements/SetsOfElements/Program.cs
@@ -0,0 +1,31 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        int n = input[0];
+        int m = input[1];
+
+        HashSet<int> leftSet = new HashSet<int>();
+        HashSet<int> rightSet = new HashSet<int>();
+
+
+        //3,5,7
+        for (int i = 0; i < n; i++)
+        {
+            int currentNum = int.Parse(Console.ReadLine());
+            leftSet.Add(currentNum);
+        }
+
+        //2,3,5
+        for (int i = 0; i < m; i++)
+        {
+            int currentNum = int.Parse(Console.ReadLine());
+            rightSet.Add(currentNum);
+        }
+
+        List<int> numbers =  leftSet.Intersect(rightSet).ToList();
+
+        Console.WriteLine(string.Join(" ",numbers));
+    }
+}
